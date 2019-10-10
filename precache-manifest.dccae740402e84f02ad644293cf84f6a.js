e92520a50f9cfdd7476abb9eefe162793fbdd26e
Bobby Schade <bschade.18@gmail.com>
2019-10-10 11:19:22 -0500

    Updates

--- precache-manifest.dccae740402e84f02ad644293cf84f6a.js
+++ precache-manifest.dccae740402e84f02ad644293cf84f6a.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "fba061bfd2e12ba0fe33eb0bf417dbba",
+    "revision": "4e1e3723d1c2bb9fd02f394fc2c8781e",
     "url": "/react-gh-calc/index.html"
   },
   {
-    "revision": "b5e29cb1b8a1e0483635",
-    "url": "/react-gh-calc/static/css/main.ac70ffc5.chunk.css"
+    "revision": "657ceedd3a59a3d3edf2",
+    "url": "/react-gh-calc/static/css/main.0716f7be.chunk.css"
   },
   {
     "revision": "283cbb3238da73526ab7",
     "url": "/react-gh-calc/static/js/2.21e67013.chunk.js"
   },
   {
-    "revision": "b5e29cb1b8a1e0483635",
-    "url": "/react-gh-calc/static/js/main.7fec3678.chunk.js"
+    "revision": "657ceedd3a59a3d3edf2",
+    "url": "/react-gh-calc/static/js/main.44351109.chunk.js"
   },
   {
     "revision": "47b8e8e39b89729f82dd",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-gh-calc/precache-manifest.6de8f3309813498bbed25837b6cfbc2c.js"
+  "/react-gh-calc/precache-manifest.dccae740402e84f02ad644293cf84f6a.js"
 );
 
 self.addEventListener('message', (event) => {
